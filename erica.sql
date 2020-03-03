b86b252651f3187c479390291bfce928b78797af
ekachner <ekibam@gmail.com>
2020-03-03 15:36:38 -0700

    added problem questions to solutions

--- erica.sql
+++ erica.sql
@@ -1,12 +1,15 @@
 USE employees;
-    
+
+-- Average age of employees   
 SELECT YEAR(NOW()) - AVG(YEAR(birth_date))
 	FROM employees;
-    
+
+-- Ratio of men vs. women    
 SELECT gender, Count(*)
 	FROM employees
     GROUP BY gender;
-    
+ 
+-- List of employees under 30
 SELECT *
 	FROM employees
     WHERE  YEAR(NOW()) - YEAR(birth_date) < 30
