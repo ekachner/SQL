b45a1bfbd307387f72d8bcb5d0478476d7f72851
ekachner <ekibam@gmail.com>
2020-03-10 10:26:17 -0600

    changed non development female salaries back to just being their original salary value

--- erica4.sql
+++ erica4.sql
@@ -4,8 +4,12 @@ use employees;
 UPDATE salaries AS s
     INNER JOIN employees AS e
 		ON e.emp_no = s.emp_no
+	INNER JOIN dept_emp AS demp
+		ON demp.emp_no = e.emp_no
+	INNER JOIN departments AS d
+		ON d.dept_no = demp.dept_no
 	SET ericas_female_raise = salary * 1.15
-	WHERE gender = "F";
+	WHERE gender = "F" AND dept_name = "Development";
     
 SELECT first_name, last_name, gender, salary, ericas_female_raise, dept_name
 	FROM employees AS e
@@ -15,7 +19,7 @@ SELECT first_name, last_name, gender, salary, ericas_female_raise, dept_name
 		ON demp.emp_no = e.emp_no
 	INNER JOIN departments AS d
 		ON d.dept_no = demp.dept_no
-	WHERE s.to_date = "9999-01-01" AND dept_name = "Development";
+	WHERE s.to_date = "9999-01-01";
     
 
 
