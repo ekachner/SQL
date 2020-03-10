248888725fcee06a37fc177e7802dd6147153cc2
ekachner <ekibam@gmail.com>
2020-03-09 21:17:35 -0600

    saved progress

--- database-playing.sql
+++ database-playing.sql
@@ -35,7 +35,11 @@ INSERT INTO students (fname, mitnitial, lname, phone, email)
     
 SELECT * 
 	FROM jason_db.students;
-    
+
+
+—————————————————————————————————————————
+
+
 DELETE
 	FROM students
     WHERE student_id IN('2', '3', '4');
@@ -44,6 +48,68 @@ DELETE
 	FROM students
     WHERE student_id = 5;
     
-DESCRIBE students;  
+DESCRIBE students;
+
+
+—————————————————————————————————————————
+
+ALTER TABLE students
+	ADD COLUMN email VARCHAR(40); 
+
+
+ALTER TABLE students
+	MODIFY COLUMN email VARCHAR(40) NOT NULL; 
+
+
+DESCRIBE students;
+
+
+INSERT INTO students (fname, minitial, lname, phone, email)
+	VALUES('Shane', 'P', 'Dominguez', '307-876-1764', 'allo@gmail.com'),
+	('Tyler', 'Q', 'Montigue', '307-846-1764', 'wallo@gmail.com'),
+    	('Zac', 'R', 'Moffat', '307-876-1714', 'marshanmallo@gmail.com'),
+   	('Bread', 'S', 'Parish', '307-287-7523', 'potatoman@haiku.com');
+
+
+DELETE
+	FROM erica_db.students
+    	WHERE student_id IN('2', '3', '4');  
+        
+—————————————————————————————————————————
     
+UPDATE table
+	SET column = expression [, column = expression]
+	[WHERE search_condition];
+
+
+
+UPDATE emp_sales
+	SET sales = sales + 10;					<— BAD   (this will change the value to “sales + 10” for EVERY “sales” column)
+
+UPDATE emp_sales
+	SET sales = sales + 10
+	WHERE emp_id = ‘E09’;					<— GOOD 
+
+
+—————————————————————————————————————————
+
+UPDATE aspiring_authors 
+	SET au_fname = 'Erica',
+		au_lname = 'Kachner'
+	WHERE au_id = 'E3';
+ 
+—————————————————————————————————————————
+  
+DELETE
+	FROM aspiring_authors
+    WHERE au_id = 'E11';
+    
+    
+    
+    
+-- ALTER TABLE salaries
+-- 	DROP COLUMN female_raise;
+
+-- ALTER TABLE `salaries`
+-- 	CHANGE COLUMN `erica_female_raise` `ericas_female_raise` INT(6);
     
--- erica4.sql
+++ erica4.sql
@@ -1,18 +1,18 @@
 use employees;
 
-ALTER TABLE salaries
-	ADD COLUMN female_raise INT(6) 
-    AFTER salary;
-    
+-- All females receive a 15% raise to their current salary    
 UPDATE salaries AS s
     INNER JOIN employees AS e
 		ON e.emp_no = s.emp_no
-	SET female_raise = salary * 1.15
+	SET ericas_female_raise = salary * 1.15
 	WHERE gender = "F";
     
-SELECT first_name, last_name, gender, salary, female_raise
+SELECT first_name, last_name, gender, salary, ericas_female_raise
 	FROM employees AS e
     INNER JOIN salaries AS s
 		ON e.emp_no = s.emp_no
-	WHERE to_date = "1999-01-01"
-	ORDER BY gender DESC;
+	WHERE to_date = "9999-01-01";
+    
+
+
+
