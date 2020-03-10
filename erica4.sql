ebe887a3ee376dbde88d9ddf5f57e2d3da2bde28
ekachner <ekibam@gmail.com>
2020-03-10 10:09:44 -0600

    added development department for female raises

--- erica4.sql
+++ erica4.sql
@@ -7,11 +7,15 @@ UPDATE salaries AS s
 	SET ericas_female_raise = salary * 1.15
 	WHERE gender = "F";
     
-SELECT first_name, last_name, gender, salary, ericas_female_raise
+SELECT first_name, last_name, gender, salary, ericas_female_raise, dept_name
 	FROM employees AS e
     INNER JOIN salaries AS s
 		ON e.emp_no = s.emp_no
-	WHERE to_date = "9999-01-01";
+	INNER JOIN dept_emp AS demp
+		ON demp.emp_no = e.emp_no
+	INNER JOIN departments AS d
+		ON d.dept_no = demp.dept_no
+	WHERE s.to_date = "9999-01-01" AND dept_name = "Development";
     
 
 
